a74c580af8b5da991fda7709827dcb37f29faa48
Ghazal <ghazalkalhori@yahoo.com>
2020-05-21 22:17:53 +0430

    board and play classes are changed - ghazal

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -1,16 +1,72 @@
 package Model;
 
+class Position {
+
+    int x, y;
+
+    Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class Board {
 
-    Cell[][] board;
+    char[][] board = new char[17][17];
+    Position posA = new Position(0, 8);
+    Position posB = new Position(16, 8);
 
-    public Board(){
-        board = new Cell[9][9];
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                board[i][j] = new Cell(false, false, false);
+    public Board() {
+        for (int i = 0; i < 17; i++) {
+            for (int j = 0; j < 17; j++) {
+                if (i % 2 == 0) {
+                    // if (j % 2 == 1)
+                    //board[i][j] = '|';
+                    if (j % 2 == 0)
+                        board[i][j] = '.';
+                }
+//                if (i % 2 == 1) {
+//                    if (j % 2 == 0)
+//                        board[i][j] = '_';
+//                    else
+//                        board[i][j] = '*';
+//                }
             }
+        }
+        board[posA.x][posA.y] = 'A'; //start point of first player
+        board[posB.x][posB.y] = 'B'; //start point of second player
+        // other cells are free in the start
+        // W char for the walls
+
+//        board[0][5]='W';
+//        board[2][5]='W';
+//
+//        board[10][1]='W';
+//        board[10][3]='W';
+    }
+
+    void movePlayer(char player, int x, int y) { // move is valid , x,y are next state
+        if (player == 'A') {
+            board[posA.x][posA.y] = '.';
+            posA.x = x;
+            posA.y = y;
+            board[posA.x][posA.y] = 'A';
+        } else if (player == 'B') {
+            board[posB.x][posB.y] = '.';
+            posB.x = x;
+            posB.y = y;
+            board[posB.x][posB.y] = 'B';
         }
     }
 
+    void putWall(int x1, int y1, int x2, int y2) //putting wall is valid
+    {
+        if (x1 == x2) {
+            board[x1][y1] = '_';
+            board[x2][y2] = '_';
+        } else if (y1 == y2) {
+            board[x1][y1] = '|';
+            board[x2][y2] = '|';
+        }
+    }
 }
--- src/Model/Play.java
+++ src/Model/Play.java
@@ -1,21 +1,128 @@
 package Model;
 
-import javafx.util.Pair;
+import java.util.Scanner;
 
 public class Play {
+    Scanner input = new Scanner(System.in);
+    Board game = new Board();
+    int x1, y1, x2, y2;
+    int currX, currY;
+    boolean turnA, turnB;
+    char player, moveType;
+//    int x = 1;
 
-
-    Player p1 = new Player(0, 5);
-    Player p2 = new Player(7, 5);
-
-
-    Board board = new Board();
-
-
-    boolean moveUp(int dir, Player p) {
-
-        return false;
+    void showState() {
+//        System.out.println("1     2     3     4     5     6     7     8     9");
+        for (int i = 0; i < 17; i++) {
+            for (int j = 0; j < 17; j++) {
+                System.out.print(game.board[i][j] + "  ");
+            }
+//            if (i % 2 == 0) {
+//                System.out.print(x);
+//                x++;
+//            }
+            System.out.println("");
+        }
     }
 
+    void getMove(char move) {
+        if (move == 'W') // put the wall
+        {
+            System.out.println("enter coordinates of the start point:");
+            x1 = input.nextInt();
+            y1 = input.nextInt();
 
+            System.out.println("enter coordinates of the start point:");
+            x2 = input.nextInt();
+            y2 = input.nextInt();
+
+            // inja bayad check konim mishe divar gozasht ya na
+            // if ok ...
+            game.putWall(x1, x2, y1, y2);
+            // else print "invlid" -> while till valid move
+        } else if (move == 'P') // move player
+        {
+            if (turnA) {
+                currX = game.posA.x;
+                currY = game.posA.y;
+                player = 'A';
+            } else if (turnB) {
+                currX = game.posB.x;
+                currY = game.posB.y;
+                player = 'B';
+            }
+            System.out.println("what type of move");
+            moveType = input.next().charAt(0);
+            switch (moveType) {
+                case 'U':
+
+                    while (currY - 1 < 0 && moveType == 'U') { // dealing with the top line
+                        System.out.println("invalid move! enter a new move");
+                        moveType = input.next().charAt(0);
+                    }
+                    if (game.board[currX][currY - 1] == 0) // cell is empty
+                        game.movePlayer(player, currX, currY - 1);
+
+                    break;
+
+                case 'D':
+
+                    while (currY + 1 > 16 && moveType == 'D') { // dealing with the bottom line
+                        System.out.println("invalid move! enter a new move");
+                        moveType = input.next().charAt(0);
+                    }
+                    if (game.board[currX][currY + 1] == 0) // cell is empty
+                        game.movePlayer(player, currX, currY + 1);
+
+                    break;
+
+                case 'L':
+
+                    while (currX - 1 < 0 && moveType == 'L') { // dealing with the left line
+                        System.out.println("invalid move! enter a new move");
+                        moveType = input.next().charAt(0);
+                    }
+                    if (game.board[currX - 1][currY] == 0) // cell is empty
+                        game.movePlayer(player, currX - 1, currY);
+
+                    break;
+
+                case 'R':
+
+                    while (currX + 1 > 16 && moveType == 'R') { // dealing with the right line
+                        System.out.println("invalid move! enter a new move");
+                        moveType = input.next().charAt(0);
+                    }
+                    if (game.board[currX + 1][currY] == 0) // cell is empty
+                        game.movePlayer(player, currX + 1, currY);
+
+                    break;
+
+                case 'S': // skew move
+                    // coordinate
+                    // check different moves
+                    break;
+            }
+        }
+//        if (goalState())
+//        {
+//            // break while
+//            // game is over
+//        }
+    }
+
+    boolean goalState() {
+        if (turnA) {
+            if (game.posA.y == 16) {
+                System.out.println("player A is winner!");
+                return true;
+            }
+        } else if (turnB) {
+            if (game.posB.y == 0) {
+                System.out.println("player B is winner!");
+                return true;
+            }
+        }
+        return false;
+    }
 }
